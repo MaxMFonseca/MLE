0920ea8cefaf9a18df18082f38daa9c48031b087
MaxMFonseca <maxmfonseca12@gmail.com>
2025-06-18 15:33:50 -0300

    Added roughtSqrt

---
+++ src/mle/common/math/Utils.cpp
@@ -0,0 +1,17 @@
+#include "Utils.h"
+
+#include "mle/common/Utils.h"
+
+namespace mle {
+// Rough square root approximation based on IEEE 754 bit hack.
+// Inspired by techniques like the Quake III fast inverse square root.
+// Public domain origins; adapted variant.
+f32 roughSqrt(f32 x) {
+    if (x == 0) {
+        return 0;
+    }
+    int i = *rAs<int*>(&x);
+    i = (i >> 1) + 0x1fc00000;  // NOLINT hack
+    return *rAs<float*>(&i);
+}
+}  // namespace mle
