4064871ac151420509f3f5f0c69ea58e4ea4c583
MaxMFonseca <maxmfonseca12@gmail.com>
2025-07-03 10:45:30 -0300

    Added a Camera class

---
+++ src/mle/renderer/Camera.cpp
@@ -0,0 +1,140 @@
+#include "Camera.h"
+
+#include <cmath>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace mle::renderer {
+void Camera::setEye(const vec3f& eye) {
+    eye_ = eye;
+    view_dirty_ = true;
+}
+
+void Camera::lookAtDir(const vec3f& dir) {
+    target_ = eye_ + glm::normalize(dir);
+    view_dirty_ = true;
+}
+
+void Camera::move(const vec3f& offset) {
+    eye_ += offset;
+    target_ += offset;
+    view_dirty_ = true;
+}
+
+void Camera::setTarget(const vec3f& target) {
+    target_ = target;
+    view_dirty_ = true;
+}
+
+void Camera::setUp(const vec3f& up) {
+    up_ = up;
+    view_dirty_ = true;
+}
+
+void Camera::rotateAroundTarget(f32 yaw_rad, f32 pitch_rad) {
+    const vec3f direction = eye_ - target_;
+    const f32 radius = glm::length(direction);
+
+    f32 theta = std::atan2(direction.z, direction.x);
+    f32 phi = std::acos(direction.y / radius);
+
+    theta += yaw_rad;
+    phi += pitch_rad;
+
+    const f32 epsilon = 0.001F;
+    phi = glm::clamp(phi, epsilon, glm::pi<f32>() - epsilon);
+
+    vec3f new_direction = {radius * sin(phi) * cos(theta), radius * cos(phi), radius * sin(phi) * sin(theta)};
+
+    eye_ = target_ + new_direction;
+    view_dirty_ = true;
+}
+
+void Camera::setFov(f32 fov_deg) {
+    fov_deg_ = fov_deg;
+    proj_dirty_ = true;
+}
+
+void Camera::setAspect(f32 aspect) {
+    aspect_ = aspect;
+    proj_dirty_ = true;
+}
+
+void Camera::setNear(f32 near_z) {
+    near_ = near_z;
+    proj_dirty_ = true;
+}
+
+void Camera::setFar(f32 far_z) {
+    far_ = far_z;
+    proj_dirty_ = true;
+}
+
+void Camera::setLeft(f32 left) {
+    left_ = left;
+    proj_dirty_ = true;
+}
+
+void Camera::setRight(f32 right) {
+    right_ = right;
+    proj_dirty_ = true;
+}
+
+void Camera::setBottom(f32 bottom) {
+    bottom_ = bottom;
+    proj_dirty_ = true;
+}
+
+void Camera::setTop(f32 top) {
+    top_ = top;
+    proj_dirty_ = true;
+}
+
+void Camera::setProjType(ProjType type) {
+    if (proj_type_ != type) {
+        proj_type_ = type;
+        proj_dirty_ = true;
+    }
+}
+
+mat4f Camera::getView() {
+    if (view_dirty_) {
+        updateView();
+    }
+    return view_;
+}
+
+mat4f Camera::getProj() {
+    if (proj_dirty_) {
+        updateProj();
+    }
+    return proj_;
+}
+
+mat4f Camera::getViewProj() {
+    if (view_proj_dirty_ || view_dirty_ || proj_dirty_) {
+        updateViewProj();
+    }
+    return view_proj_;
+}
+
+void Camera::updateView() {
+    view_ = glm::lookAt(eye_, target_, up_);
+    view_dirty_ = false;
+    view_proj_dirty_ = true;
+}
+
+void Camera::updateProj() {
+    if (proj_type_ == ProjType::PERSPECTIVE) {
+        proj_ = glm::perspective(glm::radians(fov_deg_), aspect_, near_, far_);
+    } else {
+        proj_ = glm::ortho(left_, right_, bottom_, top_, near_, far_);
+    }
+    proj_dirty_ = false;
+    view_proj_dirty_ = true;
+}
+
+void Camera::updateViewProj() {
+    view_proj_ = getProj() * getView();
+    view_proj_dirty_ = false;
+}
+}  // namespace mle::renderer
---
+++ src/mle/renderer/Camera.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "mle/common/math/Types.h"
+namespace mle::renderer {
+class Camera {
+  public:
+    enum class ProjType : u8 { PERSPECTIVE, ORTH };
+
+  public:
+    Camera(const Camera&) = default;
+    Camera(Camera&&) = default;
+    Camera& operator=(const Camera&) = default;
+    Camera& operator=(Camera&&) = default;
+
+    Camera() = default;
+    ~Camera() = default;
+
+    void setEye(const vec3f& eye);
+    void setPosition(const vec3f& position) { setEye(position); }
+    void setTarget(const vec3f& target);
+    void setUp(const vec3f& up);
+
+    void lookAtDir(const vec3f& dir);
+    void move(const vec3f& offset);
+
+    void rotateAroundTarget(f32 yaw_rad, f32 pitch_rad);
+
+    void setFov(f32 fov_deg);
+    void setAspect(f32 aspect);
+    void setNear(f32 near_z);
+    void setFar(f32 far_z);
+
+    void setLeft(f32 left);
+    void setRight(f32 right);
+    void setBottom(f32 bottom);
+    void setTop(f32 top);
+
+    void setProjType(ProjType type);
+
+    mat4f getView();
+    mat4f getProj();
+    mat4f getViewProj();
+
+    // TODO: Frustum getFrustum() const;
+
+  private:
+    void updateView();
+    void updateProj();
+    void updateViewProj();
+
+  private:
+    vec3f eye_{0.0F, 0.0F, 5.0F};
+    vec3f target_{0.0F, 0.0F, 0.0F};
+    vec3f up_{0.0F, 1.0F, 0.0F};
+
+    mat4f view_{1.0F};
+    bool view_dirty_ = true;
+
+    f32 fov_deg_ = 60.0F;
+    f32 aspect_ = 16.0F / 9.0F;
+    f32 near_ = 0.1F;
+    f32 far_ = 1000.0F;
+
+    f32 left_ = -1.0F, right_ = 1.0F, bottom_ = -1.0F, top_ = 1.0F;
+
+    ProjType proj_type_ = ProjType::PERSPECTIVE;
+
+    mat4f proj_{1.0F};
+    bool proj_dirty_ = true;
+
+    mat4f view_proj_ = mat4f(1.0F);
+    bool view_proj_dirty_ = true;
+};
+}  // namespace mle::renderer
